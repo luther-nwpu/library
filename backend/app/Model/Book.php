c6dfe2ef9c654dcb95687ed842cd2a43be9e06d4
npu2015303320 <2424733678@qq.com>
2017-09-16 10:14:03 +0800

    后端提交

---
+++ backend/app/Model/Book.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    //
+    protected $table = 'books';
+}
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('auth/login', 'Auth\AuthController@login');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
